e4ac28d38cf498da6658806ec11d77b317ee708e
vishalkundan <vishalkannnak@gmail.com>
2023-10-13 19:23:07 +0530

    cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -81,7 +81,16 @@
                     quantity: 1
                   });
                 }
-                  console.log(cart);
+
+                let cartQuantity =0;
+
+              cart.forEach((item)=>{
+                  cartQuantity += item.quantity;
+              });
+
+                document.querySelector('.js-cart-quantity')
+                  .innerHTML = cartQuantity;
+
                 
               });
           }
